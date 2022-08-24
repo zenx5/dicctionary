79a49e963fdfc0f26f26ba6d178556b6df318bc7
Octavio Martinez <omartinez1618@gmail.com>
2022-08-23 22:55:04 -0400

    rutas

---
+++ src/routes/Configure.js
@@ -0,0 +1,14 @@
+
+
+export default function Configure(){
+
+    return(
+        <ul>
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+        </ul>
+    )
+
+
+}
---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+import Dictionary from './Dictionary'
+import Configure from './Configure'
+
+export {
+    Dictionary,
+    Configure
+}
